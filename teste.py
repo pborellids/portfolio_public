1dc0dd8defa264073a588077915842f2196f5793
Paulo Borelli <141467908+pborellids@users.noreply.github.com>
2026-01-17 13:13:10 -0300

    um monte de ddddd

--- teste.py
+++ teste.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
 
-# Para definir o negativo e o positivo, o scikit-learn considera as classes na ordem alfabética por padrão.
+# Para definir o negativo e o posijjjjjjtivo, o scikit-learn considera as classes na ordem alfabética por padrão.
 # Ou seja, uma matriz de confusão 2 x 2 é entendida pelo comando confusion_matrix() desta forma:
 #  TN (modelo acertou a previsão de chamada NOK) | FP (modelo errou a previsão de chamada OK)
 #  FN (modelo errou a previsão de chamada NOK) | TP (modelo acertou a previsão de chamada OK)
