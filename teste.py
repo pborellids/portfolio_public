9d171be5b839bea4c48703d111ecaeb29fe02d32
Paulo Borelli <141467908+pborellids@users.noreply.github.com>
2026-01-17 14:46:02 -0300

    Fix comment formatting in teste.py

--- teste.py
+++ teste.py
@@ -6,7 +6,7 @@ from sklearn.metrics import confusion_matrix
 # Ou seja, uma matriz de confusão 2 x 2 é entendida pelo comando confusion_matrix() desta forma:
 #  TN (modelo acertou a previsão de chamada NOK) | FP (modelo errou a previsão de chamada OK)
 #  FN (modelo errou a previsão de chamada NOK) | TP (modelo acertou a previsão de chamada OK)
-# NNNNNNegativo = 'chamada NOK' | Positivo = 'chamada OK'
+# Negativo = 'chamada NOK' | Positivo = 'chamada OK'
 y_true = ['chamada NOK', 'chamada OK', 'chamada OK', 'chamada NOK', 'chamada NOK', 'chamada NOK']
 y_pred = ['chamada NOK','chamada OK', 'chamada OK', 'chamada NOK', 'chamada OK', 'chamada NOK']
 cm = confusion_matrix(y_true, y_pred)
